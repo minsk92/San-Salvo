0895a0c4069aca6d2c13a5d63e78c4faf8e42c68
minsk92 <minsk92@192.168.80.1>
2017-03-24 16:51:31 +0100

    asd

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,40 @@
+//funzione mostra pagine Chiesa in modo dinamico
 
+    $("#chiesa").on("pageshow", function () {
+        $.ajax("https://san-salvo-13976.firebaseio.com/Chiese.json")
+                .done(function (data) {
+                    var lista = $("#listaChiesa");
+                    lista.empty();
+                    $.map(data, function (riga, indice) {
+                        var idChiesa = 'chiesa-' + indice;
+                        var paginaChiesa = '<div data-role="page"  id="' + idChiesa + '"   style="text-align:center;">';
+                       
+                        
+                        paginaChiesa += '<div data-role="header" data-position="fixed" style="background-color:#2ad" >';
+                        paginaChiesa += "<h1>Dettaglio</h1>";
+                        paginaChiesa += "<a href='#' data-rel='back' data-icon='back' style='height:5px;'></a>";
+                        paginaChiesa += '</div>';
+                        paginaChiesa += '<div data-role="main" class="ui-content"  >';
+                        paginaChiesa += '<div>';
+                        paginaChiesa += '<h1>' + riga.nome + '</h1>';
+                        paginaChiesa += '</div>';
+                        paginaChiesa += '<img src="' + riga.foto + '" alt="" style="width:300px;">';
+                        paginaChiesa += '<div style="text-align:center;">' + riga.descr + ' </div>';
+                        paginaChiesa += '</div>';
+                        paginaChiesa += '<div data-role="footer" data-position="fixed" style="text-align: center;">';
+                        paginaChiesa += '<div data-role="controlgroup" data-type="horizontal">';
+                        paginaChiesa += '<a href="" class="ui-btn ui-btn-icon-left ui-icon-comment">Facebook</a>';
+                        paginaChiesa += '<a href="" class="ui-btn ui-btn-icon-left ui-icon-tag">Twitter</a>';
+                        paginaChiesa += '<a href="" class="ui-btn ui-btn-icon-left ui-icon-video">YouTube</a>';
+                        paginaChiesa += '</div></div>';
+                        paginaChiesa += '</div>';
+                        $('body').append(paginaChiesa);
 
-
-
-
-
-
-
-
-
-
-
-var app = {
-    
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-app.initialize();
+                        var puntoLista = '<a href="#' + idChiesa + '" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + riga.nome + '</a>';
+                        lista.append("<li>" + puntoLista + "</li>");
+                    });
+                })
+                .fail(function () {
+                    alert("Errore!");
+                });
+    });
