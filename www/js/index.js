fab700972e79bf852ffc1e9639f6399c04756983
minsk92 <minsk92@192.168.80.1>
2017-03-26 16:31:48 +0100

    asddadasf

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,17 @@
-  $("#chiesa").on("pageshow", function () {
+  $(function (){
+  $("#chiesa").on("pagecreate", function () {
         $.ajax("https://san-salvo-13976.firebaseio.com/Chiese.json")
-                .done(function (data) {
-                    var lista = $("#listaChiese");
+                .done(function(data) {
+                    console.log(data)
+                    var lista = $("#listaChiesa");
                     lista.empty();
                     $.map(data, function (riga, indice) {
-                        var idChiesa = 'chiese-' + indice;
+                        var idChiesa = indice;
                         var paginaChiesa = '<div data-role="page"  id="' + idChiesa + '"   style="text-align:center;">';
-                       
                         
                         paginaChiesa += '<div data-role="header" data-position="fixed" style="background-color:#2ad" >';
                         paginaChiesa += "<h1>Dettaglio</h1>";
                         paginaChiesa += "<a href='#' data-rel='back' data-icon='back' style='height:5px;'></a>";
-                      
-                         
                         paginaChiesa += '</div>';
                         paginaChiesa += '<div data-role="main" class="ui-content"  >';
                         paginaChiesa += '<div>';
@@ -31,7 +30,7 @@
                         paginaChiesa += '<a href="" class="ui-btn ui-btn-icon-left ui-icon-video">YouTube</a>';
                         paginaChiesa += '</div></div>';
                         paginaChiesa += '</div>';
-                        $('body').append(paginaChiesa);
+                        $("body").append(paginaChiesa);
 
                         var puntoLista = '<a href="#' + idChiesa + '" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + riga.nome + '</a>';
                         lista.append("<li>" + puntoLista + "</li>");
@@ -42,7 +41,7 @@
                 });
     });
     
-
+   });
 
 
     //funzione mostra pagine museo in modo dinamico
@@ -96,3 +95,5 @@
                 });
     });
     //funzione  Fine pagine Museo in modo dinamico
+
+ 
